38da258878ed19acc45197f1db53501d096d1f1f
Joeke Kloosterman <joekekloosterman@gmail.com>
2025-09-15 11:07:32 +0200

    Add Dutch translations

---
+++ core/components/modai/lexicon/nl/permissions.inc.php
@@ -0,0 +1,27 @@
+<?php
+// region: Admin Permissions
+$_lang['modai.permissions.modai_admin'] = 'Toegang tot modAI admin';
+$_lang['modai.permissions.modai_admin_tools'] = 'Toegang tot Tools tab';
+$_lang['modai.permissions.modai_admin_tool_save'] = 'Tools aanmaken/bijwerken';
+$_lang['modai.permissions.modai_admin_tool_delete'] = 'Tools verwijderen';
+$_lang['modai.permissions.modai_admin_context_providers'] = 'Toegang tot Context Providers tab';
+$_lang['modai.permissions.modai_admin_context_provider_save'] = 'Context providers aanmaken/bijwerken';
+$_lang['modai.permissions.modai_admin_context_provider_delete'] = 'Context providers verwijderen';
+$_lang['modai.permissions.modai_admin_agents'] = 'Toegang tot Agents tab';
+$_lang['modai.permissions.modai_admin_agent_save'] = 'Agents aanmaken/bijwerken';
+$_lang['modai.permissions.modai_admin_agent_delete'] = 'Agents verwijderen';
+$_lang['modai_admin_agent_tool_save'] = 'Tool toewijzen aan een agent';
+$_lang['modai_admin_agent_tool_delete'] = 'Tool ontkoppelen van een agent';
+$_lang['modai_admin_agent_context_provider_save'] = 'Context provider toewijzen aan een agent';
+$_lang['modai_admin_agent_context_provider_delete'] = 'Context provider ontkoppelen van een agent';
+$_lang['modai_admin_related_agent_save'] = 'Agent toewijzen aan een tool/context provider';
+$_lang['modai_admin_related_agent_delete'] = 'Agent ontkoppelen van een tool/context provider';
+// endregion: Admin Permissions
+
+// region: Client permissions
+$_lang['modai.permissions.modai_client'] = 'modAI gebruiken';
+$_lang['modai.permissions.modai_client_text'] = 'modAI tekstgeneratie gebruiken';
+$_lang['modai.permissions.modai_client_vision'] = 'modAI vision gebruiken';
+$_lang['modai.permissions.modai_client_chat_text'] = 'modAI chat gebruiken';
+$_lang['modai.permissions.modai_client_chat_image'] = 'modAI afbeeldingsgeneratie gebruiken';
+// endregion
